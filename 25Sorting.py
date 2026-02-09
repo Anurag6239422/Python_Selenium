9da6c79af5a1f5dc9074b49f888754ccb4ededb2
Anurag6239422 <anuragsandilya22678@outlook.com>
2026-02-09 10:48:15 +0530

    learning Sorting

---
+++ 25Sorting.py
@@ -0,0 +1,34 @@
+'''
+Docstring for 25Sorting
+In this Program I learn how to sort the items
+'''
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+
+driver.implicitly_wait(5)
+
+driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
+driver.maximize_window()
+
+driver.find_element(By.LINK_TEXT, "Top Deals").click()
+
+windowsOpened = driver.window_handles
+driver.switch_to.window(windowsOpened[1])
+
+driver.find_element(By.XPATH, "//span[text()='Veg/fruit name']").click()
+
+veggie = driver.find_elements(By.XPATH, "//tr//td[1]")
+
+veggie_item = []
+for item in veggie:
+    veggie_item.append(item.text)
+
+copy_veggie_item = veggie_item.copy()
+
+copy_veggie_item.sort()
+
+assert veggie_item == copy_veggie_item
